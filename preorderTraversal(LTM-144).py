ac9e2813f12ef9ae00976f2b733762df824a25eb
Abhinav <er.abhi01@gmail.com>
2020-08-17 11:17:05 +0530

    preorder traversal

---
+++ preorderTraversal(LTM-144).py
@@ -0,0 +1,32 @@
+"""
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,2,3]
+"""
+
+class Solution:
+
+    def preorderTraversal_recursive(self, root):
+        if not root: return []
+        else:
+            return [root.val]+self.preorderTraversal_recursive(root.left)+self.preorderTraversal_recursive(root.right)
+
+    def preorderTraversal_iterative(self, root):
+        if not root: return []
+        res = []
+        _stack = [root]
+        while _stack:
+            node = _stack.pop()
+            if node:
+                _stack.append(node.right)
+                _stack.append(node.left)
+        return res
